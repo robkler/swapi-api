203b0cc02705200df40eca05f3c7ead6cbafab67
Rob Kler <robklerjr@poli.ufrj.br>
2020-01-26 12:03:35 -0300

    get by id
    Signed-off-by: Rob Kler <robklerjr@poli.ufrj.br>

--- src/main.go
+++ src/main.go
@@ -31,6 +31,10 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", heartbeat).Methods("GET")
 	router.HandleFunc("/", InsertPlanet).Methods("POST")
+	router.HandleFunc("/", GetPlants).Methods("GET")
+	router.HandleFunc("/", GetByName).Methods("GET")
+	router.HandleFunc("/id/{user_uuid}", GetById).Methods("GET")
+	router.HandleFunc("/", DeletePlanet).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -40,6 +44,35 @@ func InsertPlanet(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&planet)
 	planet.Id = planet.Insert(session)
 	json.NewEncoder(w).Encode(planet)
+}
+
+
+func GetPlants(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetByName(w http.ResponseWriter, r *http.Request) {
+}
+func GetById(w http.ResponseWriter, r *http.Request) {
+	p := Planet{}
+	var err error
+	vars := mux.Vars(r)
+	uuid, err := gocql.ParseUUID(vars["user_uuid"])
+	if err != nil{
+		w.WriteHeader(412)
+		return
+	}
+	p.Id = uuid
+	fmt.Println("user_id",p.Id)
+	err = p.FindById(session)
+	if err != nil {
+		if err.Error() == "not found"{
+			w.WriteHeader(404)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(p)
+}
+func DeletePlanet(w http.ResponseWriter, r *http.Request) {
 }
 
 type Planet struct {
@@ -57,4 +90,14 @@ func (p *Planet) Insert(session *gocql.Session) gocql.UUID {
 	}
 	fmt.Println("user_id",gocql.TimeUUID())
 	return gocql.TimeUUID()
+}
+
+func (p *Planet) FindById(session *gocql.Session) error {
+	if err := session.Query(`SELECT name,climate,terrain FROM swapi.planeta WHERE id = ?`,
+		p.Id.String()).Consistency(
+		gocql.One).Scan(&p.Name,&p.Climate,&p.Terrain); err != nil {
+		return err
+	}
+	fmt.Println("user_id",gocql.TimeUUID())
+		return nil
 }
